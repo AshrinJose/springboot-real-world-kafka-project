d1fb6a4ca2df800e11f42b28533aa8c4fc07c039
Jickson George <jicksongrg@gmail.com>
2025-05-05 00:20:36 +0400

    Initial commit

---
+++ kafka-producer-wikimedia/src/main/java/com/example/WikimediaChangesProducer.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WikimediaChangesProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(){
+
+    }
+}
---
+++ src/test/java/com/example/springboot_real_world_kafka_project/SpringbootRealWorldKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_real_world_kafka_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRealWorldKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
